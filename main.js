310a0f7e49f09fe245cd49e2b5dc2c9be0ad45d8
sadiakanwal10 <sadiakanwal715@gmail.com>
2024-06-28 00:11:56 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+"use strict";
+// question no 10 //
+// SADIA KANWAL , 28/JUNE/2024 //
+// store a person name in a variable and print message to them //
+let personName = "ERIC";
+console.log(`"HELLO ${personName}, WOULD YOU LIKE TO LEARN SOME TYPESCRIPT TODAY?"`);
+//store author name in a variable and then print its favourite quote //
+let famousAuthor = "ALBERT EINTIEN";
+console.log(`"${famousAuthor} ONCE SAID THAT, LIFE IS LIKE RIDING A BICYCLE TO KEEP A BALANCE YOU MUST KEEP MOVING"`);
+// question no 11 //
+// store names in an array //
+let names = ["ayesha", "eman", "armish", "zimal"];
+// printing the name in a list by using for each method //
+names.forEach(friends => {
+    console.log(friends);
+});
+// question no 12 //
+// print names of friends in a list along with message to them // 
+names.forEach(friendsname => {
+    console.log(`"Hello ${friendsname}! how was your day today?"`);
+});
